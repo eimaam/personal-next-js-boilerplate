a36656b8055b135a3a7b1b1e8291164902faef1c
eimaam <imamddahir@gmail.com>
2024-01-02 12:38:13 +0100

    chore: update tailwind config content target location

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-primary h-screen w-screen">
+      <main className="h-screen w-screen">
         <h1>Hello World</h1>
       </main>
     </>
